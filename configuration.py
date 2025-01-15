f0ae90a2ddcfb8cd8614befab2e9b8dd662080da
Stephanie <pinoevan@gmail.com>
2025-01-15 17:48:30 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://cnt-ec65ce35-a18e-49f1-b6dd-48e97ee5098f.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
